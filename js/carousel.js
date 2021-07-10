932d45b8b346f34648435e45f269cc95b22b8554
may_may <m.pooh0917@gmail.com>
2021-07-10 20:16:09 +1000

    Add files via upload

---
+++ js/carousel.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        // appendDots: ,
+        // centerMode: true, // コメントアウトする
+        dots: false,
+        fade: true,
+        arrows: false,
+        infinite: true,
+        pauseOnHover: false,
+    });
+});
